9c7c4f523503acc9de3cb00751ffdc92a956ea7c
rlatjdfo112 <rlatjdfo112@naver.com>
2017-09-10 15:50:04 +0900

    Implement the Connect Activity

--- app/src/main/java/team/yylight/lightapplication/activity/ConnectActivity.java
+++ app/src/main/java/team/yylight/lightapplication/activity/ConnectActivity.java
@@ -2,6 +2,15 @@ package team.yylight.lightapplication.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+
+import org.json.JSONObject;
 
 import team.yylight.lightapplication.R;
 
@@ -10,11 +19,44 @@ import team.yylight.lightapplication.R;
  */
 
 public class ConnectActivity extends AppCompatActivity {
+    private View first,second,finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        first = findViewById(R.id.layout_first);
+        second = findViewById(R.id.layout_second);
+        finish = findViewById(R.id.layout_finish);
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connect(((EditText)findViewById(R.id.et_code)).getText().toString());
+            }
+        });
+    }
 
+    private void connect(String str){
+        AQuery aq = new AQuery(this);
+        first.setVisibility(View.GONE);
+        second.setVisibility(View.VISIBLE);
+        aq.ajax(getString(R.string.url_host)+getString(R.string.url_connect), JSONObject.class, new AjaxCallback<JSONObject>(){
+            @Override
+            public void callback(String url, JSONObject object, AjaxStatus status) {
+                second.setVisibility(View.GONE);
+                if(status.getCode() >= 200){
+                    finish.setVisibility(View.VISIBLE);
+                }else{
+                    Toast.makeText(ConnectActivity.this, "기기 연결에 실패했습니다!", Toast.LENGTH_SHORT).show();
+                    first.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/team/yylight/lightapplication/activity/items/ItemsActivity.java
+++ app/src/main/java/team/yylight/lightapplication/activity/items/ItemsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -38,11 +39,31 @@ public class ItemsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
 
-        et_search = (EditText)findViewById(R.id.et_search);
+        et_search = (EditText) findViewById(R.id.et_search);
+        et_search.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)&&et_search.getText().toString().length()>0) {
+                    AQuery aq = new AQuery(ItemsActivity.this);
+                    aq.ajax(getString(R.string.url_host) + getString(R.string.url_search_tag) + "?tags=" + et_search.getText().toString().replaceAll(",", "+"), JSONArray.class, new AjaxCallback<JSONArray>() {
+                        @Override
+                        public void callback(String url, JSONArray arr, AjaxStatus status) {
+                            if (status.getCode() == 200) {
+                                loadLightItems(arr);
+                            } else {
+                                Log.e("ItemsActivity", status.getMessage() + status.getError());
+                            }
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
+        });
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         View view = getLayoutInflater().inflate(R.layout.actionbar_title_view, null);
-        ((TextView)view.findViewById(R.id.tv_title)).setText("Items");
+        ((TextView) view.findViewById(R.id.tv_title)).setText("Items");
         view.findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,7 +71,7 @@ public class ItemsActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setCustomView(view);
-        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -85,24 +106,26 @@ public class ItemsActivity extends AppCompatActivity {
         }
     }
 
-    private void loadLightItems(){
+    private void loadLightItems(JSONArray arr){
+        adapter.clear();
+        for (int i = 0; i < arr.length(); i++) {
+            try {
+                JSONObject jsonObject = arr.getJSONObject(i);
+                adapter.add(new LightItem(jsonObject.getInt("id"), jsonObject.getString("author"), jsonObject.getString("title"), jsonObject.getString("content"), getResources().getString(R.string.url_host) + jsonObject.getString("thumbnail"), jsonObject.getDouble("score"), jsonObject.getInt("amount"), jsonObject.getString("temperature"), jsonObject.getJSONArray("tags")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void loadLightItems() {
         AQuery aq = new AQuery(ItemsActivity.this);
-        aq.ajax(getResources().getString(R.string.url_host)+getResources().getString(R.string.url_load_items),JSONArray.class,  new AjaxCallback<JSONArray>(){
-            public void callback(String url, JSONArray arr, AjaxStatus status)
-            {
-                adapter.clear();
-                if(status.getCode()==200) {
-                    for (int i = 0; i < arr.length(); i++) {
-                        try {
-                            JSONObject jsonObject = arr.getJSONObject(i);
-                            Log.d("Test", "" + arr.length());
-                            adapter.add(new LightItem(jsonObject.getInt("id"), jsonObject.getString("author"), jsonObject.getString("title"), jsonObject.getString("content"), getResources().getString(R.string.url_host) + jsonObject.getString("thumbnail"), jsonObject.getDouble("score"), jsonObject.getInt("amount"), jsonObject.getString("temperature"), jsonObject.getJSONArray("tags")));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }else{
-                    Log.e("ItemsActivity", status.getMessage()+status.getError());
+        aq.ajax(getResources().getString(R.string.url_host) + getResources().getString(R.string.url_load_items), JSONArray.class, new AjaxCallback<JSONArray>() {
+            public void callback(String url, JSONArray arr, AjaxStatus status) {
+                if (status.getCode() == 200) {
+                    loadLightItems(arr);
+                } else {
+                    Log.e("ItemsActivity", status.getMessage() + status.getError());
                 }
             }
         });
